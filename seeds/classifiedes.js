a83fe4655f184bf26a112d7df9d64209f0721f4e
Justyna Foster <justynawfoster@gmail.com>
2017-02-08 20:39:35 -0700

    update jshint

--- seeds/classifiedes.js
+++ seeds/classifiedes.js
@@ -7,21 +7,39 @@ exports.seed = function(knex, Promise) {
                     // Inserts seed entries
                     knex('classifieds').insert({
                         id: 1,
-                        title: 'NES Classic',
-                        description: 'I got lucky and found it, and decided to charge 10x what it was worth.',
-                        price: 600,
-                        item_image: 'http://www.nintendo.com/images/page/nes-classic/nes-classic-edition-box.png',
+                        title: '2012 Cannondale Synapse Hi-Mod Dura Ace',
+                        description: 'Brand new bike. less than 50 miles ridden. I am selling for my father who is buying a racier bike. This is a TOP of the LINE RACE BIKE.',
+                        price: 4750,
+                        item_image: 'http://www.probikeswap.com/wp-content/uploads/2013/03/164262-500x333.jpg',
                         created_at: '2016-06-26T14:26:16.000Z',
                         updated_at: '2016-06-26T14:26:16.000Z'
                     }),
                     knex('classifieds').insert({
                         id: 2,
-                        title: 'Pikachu 9" Plush Stuffed Toy',
-                        description: 'Polyester fiber construction Officially licensed.',
-                        price: 10,
-                        item_image: 'https://images-na.ssl-images-amazon.com/images/I/41VwGotRZsL._SY300_.jpg',
-                        created_at: '2016-06-26T14:26:16.000Z',
-                        updated_at: '2016-06-26T14:26:16.000Z'
+                        title: 'Schwinn Fastback Pro XL',
+                        description: 'Bike is mechanically perfect. Tires will need to be replaced soon, and the shifters show some wear. Otherwise, it’s ready to ride. ',
+                        price: 800,
+                        item_image: 'http://www.probikeswap.com/wp-content/uploads/2015/01/874743-500x375.jpg',
+                        created_at: '2017-02-03T14:26:16.000Z',
+                        updated_at: '2017-02-04T14:26:16.000Z'
+                    }),
+                    knex('classifieds').insert({
+                        id: 3,
+                        title: 'Cannondale Bad Boy – Flat Bar Race Bike',
+                        description: 'Last of the Bad Boys to be built in the USA. I bought this for the frame in ’09 and before riding it replaced every component. Every one that is, but the Fatty D Head Shock.',
+                        price: 2100,
+                        item_image: 'http://www.probikeswap.com/wp-content/uploads/2016/04/882820-500x334.jpg',
+                        created_at: '2016-12-26T14:26:16.000Z',
+                        updated_at: '2017-01-26T14:26:16.000Z'
+                    }),
+                    knex('classifieds').insert({
+                        id: 4,
+                        title: 'JAPANESE SHIMURA SEIKI CO ROAD PUPPY',
+                        description: 'JAPANESE SHIMURA SEIKI CO ROAD PUPPY ALL ORIGNAL EXCEPT IT WAS PAINTED SOMEWHERE ALONG THE LINE',
+                        price: 500,
+                        item_image: 'http://www.probikeswap.com/wp-content/uploads/2014/12/310487-500x373.jpg',
+                        created_at: '2017-01-26T14:26:16.000Z',
+                        updated_at: '2017-02-06T14:26:16.000Z'
                     })
                 ])
                 .then(() => {
