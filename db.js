c9953b90c7117ddc183f0f66af1696a865422c5d
Justyna Foster <justynawfoster@gmail.com>
2017-02-09 14:00:35 -0700

    fixing bug

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 'use strict';
-var config = require('../knexfile.js')[process.env.NODE_ENV || 'development']);
+var config = require('../knexfile.js')[process.env.NODE_ENV || 'development'];
 var env = 'development';
 var knex = require('knex')(config[env]);
 
